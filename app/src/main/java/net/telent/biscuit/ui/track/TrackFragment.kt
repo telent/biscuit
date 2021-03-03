a0b8ecae49e9ecc432908d44d3dedfb28ab15974
Daniel Barlow <dan@telent.net>
2021-03-03 23:29:28 +0000

    improved cyclist "avatar" icon
    also make a dragged map stay dragged until the unit moves again

--- app/src/main/java/net/telent/biscuit/ui/track/TrackFragment.kt
+++ app/src/main/java/net/telent/biscuit/ui/track/TrackFragment.kt
@@ -1,22 +1,27 @@
 package net.telent.biscuit.ui.track
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import net.telent.biscuit.R
+import net.telent.biscuit.Trackpoint
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.IconOverlay
 import org.osmdroid.views.overlay.Polyline
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class TrackFragment : Fragment() {
     private lateinit var map : MapView
+    private lateinit var youAreHere: IconOverlay
     private val model: TrackViewModel by activityViewModels()
+    private var previousLat : Double = 0.0
+    private var previousLng : Double = 0.0
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -28,7 +33,7 @@ class TrackFragment : Fragment() {
         map = root.findViewById(R.id.map)
 
         // this will be replaced by our track marker.
-        // we add it first so we know we can replae it at position 0
+        // we add it first so we know we can replace it at position 0
         map.overlayManager.add(Polyline())
 
         map.setTileSource(TileSourceFactory.MAPNIK)
@@ -36,18 +41,18 @@ class TrackFragment : Fragment() {
         map.zoomController.setZoomOutEnabled(true)
         map.setMultiTouchControls(true)
 
-        val gpsMyLocationProvider = GpsMyLocationProvider(this.requireActivity().baseContext).let {
-            it.locationUpdateMinDistance = 1.0f // [m]  // Set the minimum distance for location updates
-            it.locationUpdateMinTime = 1000
-            it
-        }
-        val locOverlay = MyLocationNewOverlay(gpsMyLocationProvider, map)
-        map.overlays.add(locOverlay)
+        val icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_bike_abov)
+        youAreHere = IconOverlay(GeoPoint(0.0,0.0), icon)
+        map.overlays.add(youAreHere)
 
-        map.controller.setZoom(18.0)
+        map.controller.setZoom(20.0)
         model.trackpoint.observe(viewLifecycleOwner, {
-            if(it.lat != null && it.lng != null)
-                map.controller.setCenter(GeoPoint(it.lat.toDouble(), it.lng.toDouble()))
+            if (haveMoved(it)) {
+                Log.d("moved", " ${map.mapCenter} ${it}")
+                val loc = GeoPoint(it.lat!!.toDouble(), it.lng!!.toDouble())
+                youAreHere.moveTo(loc, map)
+                map.controller.setCenter(loc)
+            }
         })
         model.track.observe(viewLifecycleOwner ,{
             val line = Polyline()
@@ -55,6 +60,16 @@ class TrackFragment : Fragment() {
             map.overlayManager[0] = line
         })
         return root
+    }
+
+    private fun haveMoved( it: Trackpoint): Boolean {
+        return if(it.lat != null && it.lng != null) {
+            val moved = it.lat != previousLat || it.lng != previousLng
+            previousLat = it.lat
+            previousLng = it.lng
+            moved
+        } else
+            false
     }
 
     override fun onResume() {
