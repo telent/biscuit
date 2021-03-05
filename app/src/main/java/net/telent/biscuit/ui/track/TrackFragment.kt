caf5e55a30aaa2e4f947261d8f963bbefe497449
Daniel Barlow <dan@telent.net>
2021-03-05 14:11:59 +0000

    rotate the cyclist avatar to point in direction of travel

--- app/src/main/java/net/telent/biscuit/ui/track/TrackFragment.kt
+++ app/src/main/java/net/telent/biscuit/ui/track/TrackFragment.kt
@@ -1,5 +1,7 @@
 package net.telent.biscuit.ui.track
 
+import android.graphics.Canvas
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,6 +15,7 @@ import net.telent.biscuit.Trackpoint
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.Projection
 import org.osmdroid.views.overlay.IconOverlay
 import org.osmdroid.views.overlay.Polyline
 
@@ -32,6 +35,8 @@ class TrackFragment : Fragment() {
 
         map = root.findViewById(R.id.map)
 
+        var bearing : Float? = null
+
         // this will be replaced by our track marker.
         // we add it first so we know we can replace it at position 0
         map.overlayManager.add(Polyline())
@@ -42,13 +47,20 @@ class TrackFragment : Fragment() {
         map.setMultiTouchControls(true)
 
         val icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_bike_abov)
-        youAreHere = IconOverlay(GeoPoint(0.0,0.0), icon)
+        youAreHere = object : IconOverlay(GeoPoint(0.0,0.0), icon) {
+            override fun draw(c : Canvas, p : Projection) {
+                val b = bearing ?: null
+                if(b!= null)  this.mBearing = b
+                super.draw(c, p)
+            }
+        }
         map.overlays.add(youAreHere)
 
         map.controller.setZoom(20.0)
         model.trackpoint.observe(viewLifecycleOwner, {
-            if (haveMoved(it)) {
-                Log.d("moved", " ${map.mapCenter} ${it}")
+            bearing = bearingTo(it)
+            if (bearing != null) {
+                Log.d("moved", " ${map.mapCenter} $it $bearing")
                 val loc = GeoPoint(it.lat!!.toDouble(), it.lng!!.toDouble())
                 youAreHere.moveTo(loc, map)
                 map.controller.setCenter(loc)
@@ -62,14 +74,22 @@ class TrackFragment : Fragment() {
         return root
     }
 
-    private fun haveMoved( it: Trackpoint): Boolean {
-        return if(it.lat != null && it.lng != null) {
-            val moved = it.lat != previousLat || it.lng != previousLng
-            previousLat = it.lat
-            previousLng = it.lng
-            moved
-        } else
-            false
+    private fun bearingTo(tp: Trackpoint): Float? {
+        if (tp.lat != null && tp.lng != null) {
+            val from = Location("none").apply {
+                latitude = previousLat
+                longitude = previousLng
+            }
+            val to = Location("none").apply {
+                latitude = tp.lat; longitude = tp.lng
+            }
+            if (from.distanceTo(to) > 1) {
+                previousLat = tp.lat
+                previousLng = tp.lng
+                return from.bearingTo(to)
+            }
+        }
+        return null
     }
 
     override fun onResume() {
